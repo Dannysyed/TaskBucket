d503a50ff1713422830a4577d07b4dd4ab03cd88
Dannysyed <dannysyed20@gmail.com>
2024-05-29 19:25:26 -0400

    Routes in frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
+import LoginPage from "./components/Auth/Login/LoginPage";
+import SignupPage from "./components/Auth/Signup/SignupPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign" element={<SignupPage />} />
         <Route path="/NotFound" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
